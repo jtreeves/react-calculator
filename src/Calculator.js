8eaadbf7f8d716475a75e7b1c85667cad046e0b1
jtreeves <jackson.t.reeves@gmail.com>
2020-12-09 22:33:51 -0500

    [js] Delete props from parentheses

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 
 class Calculator extends Component {
-    constructor(props) {
-        super(props)
+    constructor() {
+        super()
         this.state = {
             number1: '',
             number2: '',
