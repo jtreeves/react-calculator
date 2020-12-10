ef6aef225d79d879d3d98ae6dd31bfbfd6cf3bad
jtreeves <jackson.t.reeves@gmail.com>
2020-12-09 21:54:52 -0500

    [js] Add trigger for adding

--- src/Calculator.js
+++ src/Calculator.js
@@ -8,14 +8,30 @@ class Calculator extends Component {
         }
     }
 
+    setNum = (e, num) => {
+        this.setState({ [num]: e.target.value})
+    }
+
     render() {
         return (
-            <div className="container">
+            <div className='container'>
                 <h1>Add with React!</h1>
-                <div className="add">
-                    <input type="text" />
+                <div className='add'>
+                    <input
+                        type='number'
+                        name='num1'
+                        placeholder='Enter your first number'
+                        value={this.state.num1}
+                        onChange={ (e) => this.setNum(e, 'num1') }
+                    />
                     <span>+</span>
-                    <input type="text" />
+                    <input
+                        type='number'
+                        name='num2'
+                        placeholder='Enter your second number'
+                        value={this.state.num2}
+                        onChange={ (e) => this.setNum(e, 'num2') }
+                    />
                     <button>=</button>
                     <h3>Addition results go here!</h3>
                 </div>
